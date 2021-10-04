0173a47d1db2494675e321b295e3254e8ebc63bf
CS <kstasyuk@ya.ru>
2021-10-04 23:04:49 +0300

    4. Скачивание файла с ограничением

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,21 +1,44 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import java.io.*;
+import java.net.URL;
+import java.time.LocalTime;
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(300);
-                        }
-                        System.out.println(System.lineSeparator() + "Main started");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+public class Wget implements Runnable {
+
+    private final String url;
+    private final long speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed * 1000L;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long timeMark = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long delay = System.currentTimeMillis() - timeMark;
+                if (delay < speed) {
+                    System.out.println("sleep " + (speed - delay));
+                    Thread.sleep(speed - delay);
                 }
-        );
-        thread.start();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
